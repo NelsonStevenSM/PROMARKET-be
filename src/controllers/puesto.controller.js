72ed89f4fbf5da668a9b104452c0ee715462f6d3
Claudio Cervantes Giron <claudiomauricio_23@hotmail.com>
2023-01-31 23:22:06 -0500

    Se agregan requerimientos

--- src/controllers/puesto.controller.js
+++ src/controllers/puesto.controller.js
@@ -61,7 +61,7 @@ const getDataPuesto = async (req, res) => {
             response.cliente = cliente
         }
 
-        if ( (estadoPuesto === 0 || estadoPuesto === 2) && (financiamiento_id !== null && financiamiento_id !== undefined)) {
+        if ((estadoPuesto === 0 || estadoPuesto === 2) && (financiamiento_id !== null && financiamiento_id !== undefined)) {
             valueFinanciamiento.push(financiamiento_id)
             const responseFinanciamiento = await pool.query(scriptFinanciamiento, valueFinanciamiento)
             response.financiamiento = responseFinanciamiento.rows[0]
@@ -82,129 +82,204 @@ const createDataPuesto = async (req, res) => {
     conyuge = cliente.conyuge
     vendedor_id = cliente.vendedor.dni
 
-    const scriptPuesto = 'SELECT * FROM "PUESTOS" WHERE ID = $1'
-    const scriptPuestoUpdate = 'UPDATE "PUESTOS" SET nro_local = $1, ancho = $2, largo = $3, previo_venta = $4, estado = $5, fk_cliente = $6, fk_financiamiento = $7, comentario = $8 WHERE id = $9'
-    const scriptCliente = 'INSERT INTO "CLIENTES"(dni, nombre, apaterno, amaterno, celular, correo, estado_civil, direccion, fk_conyuge, fk_usu_venta) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);'
-    const scriptFinanciamiento = 'INSERT INTO "FINANCIAMIENTOS"(id, imp_separacion, saldo_inicial, financiamiento, fecha_separacion, fecha_saldo_inicial, fecha_financiamiento) VALUES ($1, $2, $3, $4, $5, $6, $7);'
-    const scriptConyuge = 'INSERT INTO "CONYUGES"(dni, nombre, apaterno, amaterno, celular, correo) VALUES ($1, $2, $3, $4, $5, $6);'
-    const scriptVendedor = 'SELECT * FROM "USUARIOS" WHERE DNI = $1'
+    //SCRIPTS BUSQUEDA
+    const scriptSearchPuesto = 'SELECT * FROM "PUESTOS" WHERE ID = $1'
+    const scriptSearchConyuge = 'SELECT * FROM "CONYUGES" WHERE DNI = $1';
+    const scriptSearchVendedor = 'SELECT * FROM "USUARIOS" WHERE DNI = $1'
+    const scriptSearchCliente = 'SELECT * FROM "CLIENTES" WHERE DNI = $1'
+    const scriptSearchFinanciamiento = 'SELECT * FROM "FINANCIAMIENTOS" WHERE ID = $1'
 
+    //SCRIPTS CREACION
+    const scriptCreateConyuge = 'INSERT INTO "CONYUGES"(dni, nombre, apaterno, amaterno, celular, correo) VALUES ($1, $2, $3, $4, $5, $6);'
+    const scriptCreateCliente = 'INSERT INTO "CLIENTES"(dni, nombre, apaterno, amaterno, celular, correo, estado_civil, direccion, fk_conyuge, fk_usu_venta) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);'
+    const scriptCreateFinanciamiento = 'INSERT INTO "FINANCIAMIENTOS"(id, imp_separacion, saldo_inicial, financiamiento, fecha_separacion, fecha_saldo_inicial, fecha_financiamiento) VALUES ($1, $2, $3, $4, $5, $6, $7);'
+
+    //SCRIPTS UPDATE
+    const scriptUpdateConyuge = 'UPDATE "CONYUGES" SET nombre = $1, apaterno = $2, amaterno = $3, celular = $4, correo = $5 WHERE dni = $6'
+    const scriptUpdateCliente = 'UPDATE "CLIENTES" SET nombre = $1, apaterno = $2, amaterno = $3, celular = $4, correo = $5, estado_civil = $6, direccion = $7, fk_conyuge = $8, fk_usu_venta = $9 WHERE dni = $10;'
+    const scriptUpdateFinanciamiento = 'UPDATE "FINANCIAMIENTOS" SET imp_separacion = $1, saldo_inicial = $2, financiamiento = $3, fecha_separacion = $4, fecha_saldo_inicial = $5, fecha_financiamiento = $6 WHERE id = $7;'
+    const scriptUpdatePuesto = 'UPDATE "PUESTOS" SET nro_local = $1, ancho = $2, largo = $3, previo_venta = $4, estado = $5, fk_cliente = $6, fk_financiamiento = $7, comentario = $8 WHERE id = $9'
+
+    //VALORES CREACION / ACTUALIZACION
     const valuePuesto = []
-    const valuePuestoUpdate = []
-    const valueCliente = []
-    const valueFinanciamiento = []
     const valueConyuge = []
     const valueVendedor = []
-
+    const valueCliente = []
+    const valueFinanciamiento = []
+    
     conyuge_id = null
     cliente_id = null
 
     if (puesto_id !== null || puesto_id !== undefined) {
-        //BUSCAMOS VENDEDOR
+
+        //BUSCAMOS PUESTO
         valuePuesto.push(puesto_id)
+        const rsSearchPuesto = await pool.query(scriptSearchPuesto, valuePuesto)
 
-        const responsePuestoExiste = await pool.query(scriptPuesto, valuePuesto)
-
-        if (responsePuestoExiste.rows.length === 0) {
-            res.status(400).json({"error":"No se encontro puesto especificado"})
+        if (rsSearchPuesto.rows.length === 0) {
+            res.status(400).json({ "error": "No se encontro puesto especificado" })
         }
 
+        //CONYUGE
         if (conyuge !== null && conyuge !== undefined) {
-            //CREACION DE CONYUGE
+            //BUSCAMOS CONYUGE EN BASE DE DATOS
             valueConyuge.push(conyuge.dni)
-            valueConyuge.push(conyuge.nombre)
-            valueConyuge.push(conyuge.apaterno)
-            valueConyuge.push(conyuge.amaterno)
-            valueConyuge.push(conyuge.celular)
-            valueConyuge.push(conyuge.correo)
+            const rsSearchConyuge = await pool.query(scriptSearchConyuge, valueConyuge)
+
+            if (rsSearchConyuge.rows.length === 0 || rsSearchConyuge.rows.length === 1) {
+                //INICIAMOS VALORES CONYUGE
+                valueConyuge.push(conyuge.nombre)
+                valueConyuge.push(conyuge.apaterno)
+                valueConyuge.push(conyuge.amaterno)
+                valueConyuge.push(conyuge.celular)
+                valueConyuge.push(conyuge.correo)
+
+                if (rsSearchConyuge.rows.length === 0) {
+                    // CREACION DE CONYUGE
+                    const responseConyuge = await pool.query(scriptCreateConyuge, valueConyuge)
     
-            const responseConyuge = await pool.query(scriptConyuge, valueConyuge)
+                    if (responseConyuge.rowCount !== 1) {
+                        res.status(400).json({ "error": "Ocurrio un error al crear conyuge" })
+                    }
+                }
     
-            if (responseConyuge.rowCount !== 1) {
-                res.status(400).json({"error":"Ocurrio un error al crear conyuge"})
+                if (rsSearchConyuge.rows.length === 1) {
+                    // ACTUALIZACION DE CONYUGE
+                    valueConyuge.push(conyuge.dni)
+                    valueConyuge.splice(0, 1)
+                    const responseConyuge = await pool.query(scriptUpdateConyuge, valueConyuge)
+    
+                    if (responseConyuge.rowCount !== 1) {
+                        res.status(400).json({ "error": "Ocurrio un error al actualizar conyuge" })
+                    }
+                }
+
+                conyuge_id = conyuge.dni
+            } else {
+                res.status(400).json({ "error": "Ocurrio un error al buscar conyuge" })
             }
-    
-            conyuge_id = conyuge.dni
         }
-    
+
+        //VENDEDOR
         if (vendedor_id !== null && vendedor_id !== undefined) {
             //BUSCAMOS VENDEDOR
             valueVendedor.push(vendedor_id)
-    
-            const responseConyuge = await pool.query(scriptVendedor, valueVendedor)
-    
-            if (responseConyuge.rows.length === 0) {
-                res.status(400).json({"error":"No se encontro vendedor con dni especificado"})
+            const rsVendedor = await pool.query(scriptSearchVendedor, valueVendedor)
+
+            if (rsVendedor.rows.length === 0) {
+                res.status(400).json({ "error": "No se encontro vendedor con dni especificado" })
             }
         } else {
-            res.status(400).json({"error":"El vendedor no puede ser vacio"})
+            res.status(400).json({ "error": "El vendedor no puede ser vacio" })
         }
-    
+
+        //CLIENTE
         if (cliente !== null && cliente !== undefined) {
-            //CREACION DE CONYUGE
-            valueCliente.push(cliente.dni)
-            valueCliente.push(cliente.nombre)
-            valueCliente.push(cliente.apaterno)
-            valueCliente.push(cliente.amaterno)
-            valueCliente.push(cliente.celular)
-            valueCliente.push(cliente.correo)
-            valueCliente.push(cliente.estado_civil)
-            valueCliente.push(cliente.direccion)
-            valueCliente.push(conyuge_id)
-            valueCliente.push(vendedor_id)
+            //BUSCAMOS CLIENTE EN BASE DE DATOS
+            valueCliente.push(cliente.dni) //VALUE 1
+            const rsSearchCliente = await pool.query(scriptSearchCliente, valueCliente)
+
+            if (rsSearchCliente.rows.length === 0 || rsSearchCliente.rows.length === 1) {
+                //INICIAMOS VALORES CLIENTE
+                valueCliente.push(cliente.nombre)
+                valueCliente.push(cliente.apaterno)
+                valueCliente.push(cliente.amaterno)
+                valueCliente.push(cliente.celular)
+                valueCliente.push(cliente.correo)
+                valueCliente.push(cliente.estado_civil)
+                valueCliente.push(cliente.direccion)
+                valueCliente.push(conyuge_id)
+                valueCliente.push(vendedor_id)
+
+                if (rsSearchCliente.rows.length === 0) {
+                    // CREACION DE CLIENTE
+                    const responseCliente = await pool.query(scriptCreateCliente, valueCliente)
     
-            const responseCliente = await pool.query(scriptCliente, valueCliente)
+                    if (responseCliente.rowCount !== 1) {
+                        res.status(400).json({ "error": "Ocurrio un error al crear cliente" })
+                    }
+                }
     
-            if (responseCliente.rowCount !== 1) {
-                res.status(400).json({"error":"Ocurrio un error al crear cliente"})
+                if (rsSearchCliente.rows.length === 1) {
+                    // ACTUALIZACION DE CLIENTE
+                    valueCliente.push(cliente.dni)
+                    valueCliente.splice(0, 1)
+                    const responseCliente = await pool.query(scriptUpdateCliente, valueCliente)
+    
+                    if (responseCliente.rowCount !== 1) {
+                        res.status(400).json({ "error": "Ocurrio un error al actualizar cliente" })
+                    }
+                }
+
+                cliente_id = cliente.dni
+            } else {
+                res.status(400).json({ "error": "Ocurrio un error al buscar cliente" })
             }
-    
-            cliente_id = cliente.dni
+            
         } else {
-            res.status(400).json({"error":"El cliente no puede ser vacio"})
+            res.status(400).json({ "error": "El cliente no puede ser vacio" })
         }
-    
+
+        //FINANCIAMIENTO
         if (financiamiento !== null && financiamiento !== undefined) {
+            //BUSCAMOS CLIENTE EN BASE DE DATOS
+            valueFinanciamiento.push(puesto_id + '_F') //VALUE 1
+            const rsSearchFinanciamiento = await pool.query(scriptSearchFinanciamiento, valueFinanciamiento)
 
-            // financiamiento.financiamiento = parseInt(financiamiento.financiamiento.substr(3));
-            // console.log(financiamiento.financiamiento);
+            if (rsSearchFinanciamiento.rows.length === 0 || rsSearchFinanciamiento.rows.length === 1) {
+                //INICIAMOS VALORES FINANCIAMIENTO
+                valueFinanciamiento.push(financiamiento.imp_separacion)
+                valueFinanciamiento.push(financiamiento.saldo_inicial)
+                valueFinanciamiento.push(financiamiento.financiamiento)
+                valueFinanciamiento.push(financiamiento.fecha_separacion)
+                valueFinanciamiento.push(financiamiento.fecha_saldo_inicial)
+                valueFinanciamiento.push(financiamiento.fecha_financiamiento)
 
-            //CREACION DE CONYUGE
-            valueFinanciamiento.push(puesto_id + '_F')
-            valueFinanciamiento.push(financiamiento.imp_separacion)
-            valueFinanciamiento.push(financiamiento.saldo_inicial)
-            valueFinanciamiento.push(financiamiento.financiamiento)
-            valueFinanciamiento.push(financiamiento.fecha_separacion)
-            valueFinanciamiento.push(financiamiento.fecha_saldo_inicial)
-            valueFinanciamiento.push(financiamiento.fecha_financiamiento)
+                if (rsSearchFinanciamiento.rows.length === 0) {
+                    // CREACION DE CLIENTE
+                    const responseFinanciamiento = await pool.query(scriptCreateFinanciamiento, valueFinanciamiento)
     
-            const responseFinanciamiento = await pool.query(scriptFinanciamiento, valueFinanciamiento)
+                    if (responseFinanciamiento.rowCount !== 1) {
+                        res.status(400).json({ "error": "Ocurrio un error al crear financiamiento" })
+                    }
+                }
     
-            if (responseFinanciamiento.rowCount !== 1) {
-                res.status(400).json({"error":"Ocurrio un error al crear financiamiento"})
+                if (rsSearchFinanciamiento.rows.length === 1) {
+                    // ACTUALIZACION DE CLIENTE
+                    valueFinanciamiento.push(puesto_id + '_F') //VALUE 8
+                    valueFinanciamiento.splice(0, 1)
+                    const responseFinanciamiento = await pool.query(scriptUpdateFinanciamiento, valueFinanciamiento)
+    
+                    if (responseFinanciamiento.rowCount !== 1) {
+                        res.status(400).json({ "error": "Ocurrio un error al actualizar financiamiento" })
+                    }
+                }
+            } else {
+                res.status(400).json({ "error": "Ocurrio un error al buscar financiamiento" })
             }
         } else {
-            res.status(400).json({"error":"El financiamiento no puede ser vacio"})
+            res.status(400).json({ "error": "El financiamiento no puede ser vacio" })
         }
 
-        valuePuestoUpdate.push(puesto.nro_local)
-        valuePuestoUpdate.push(puesto.ancho)
-        valuePuestoUpdate.push(puesto.largo)
-        valuePuestoUpdate.push(puesto.previo_venta)
-        valuePuestoUpdate.push(puesto.estado)
-        valuePuestoUpdate.push(cliente_id)
-        valuePuestoUpdate.push(puesto_id + '_F')
-        valuePuestoUpdate.push(comentario)
-        valuePuestoUpdate.push(puesto_id)
-        const responsePuestoUpdate = await pool.query(scriptPuestoUpdate, valuePuestoUpdate)
+        valuePuesto.push(puesto.nro_local)
+        valuePuesto.push(puesto.ancho)
+        valuePuesto.push(puesto.largo)
+        valuePuesto.push(puesto.previo_venta)
+        valuePuesto.push(puesto.estado)
+        valuePuesto.push(cliente_id)
+        valuePuesto.push(puesto_id + '_F')
+        valuePuesto.push(comentario)
+        valuePuesto.push(puesto_id)
+        valuePuesto.splice(0, 1)
+        const responsePuestoUpdate = await pool.query(scriptUpdatePuesto, valuePuestoUpdate)
 
         if (responsePuestoUpdate.rowCount !== 1) {
-            res.status(400).json({"error":"Ocurrio un error al actualizar puesto"})
+            res.status(400).json({ "error": "Ocurrio un error al actualizar puesto" })
         } else {
             res.status(200).json(responsePuestoUpdate.rowCount)
         }
     } else {
-        res.status(400).json({"error":"El puesto no puede ser vacio"})
+        res.status(400).json({ "error": "El puesto no puede ser vacio" })
     }
 }
 
--- src/controllers/reporte.controller.js
+++ src/controllers/reporte.controller.js
@@ -4,7 +4,7 @@ const getPuestosVendidos = async (req, res) => {
     try {
         const nombre_completo = "(C.nombre || ' ' || C.apaterno)"
         const vendedor = "(U.nombre || ' ' || U.apaterno)"
-        const response = await pool.query('SELECT P.id AS "ID", P.nro_local AS "NRO_LOCAL", C.dni AS "DNI", ' + nombre_completo + ' AS "CLIENTE", C.celular AS "CELULAR", C.correo AS "CORREO", F.imp_separacion AS "IMP_SEPARACION", F.saldo_inicial AS "SALDO_INICIAL", F.financiamiento AS "FINANCIAMIENTO", ' + vendedor + ' AS "VENDEDOR", P.comentario AS "COMENTARIO" FROM "PUESTOS" P, "CLIENTES" C, "FINANCIAMIENTOS" F, "USUARIOS" U WHERE P.fk_cliente = C.dni AND C.fk_usu_venta = U.dni AND P.fk_financiamiento = F.id');
+        const response = await pool.query('SELECT P.id AS "ID", P.nro_local AS "NRO_LOCAL", C.dni AS "DNI", ' + nombre_completo + ' AS "CLIENTE", C.celular AS "CELULAR", P.previo_venta AS "PRECIO_VENTA", C.correo AS "CORREO", F.imp_separacion AS "IMP_SEPARACION", F.saldo_inicial AS "SALDO_INICIAL", F.financiamiento AS "FINANCIAMIENTO", (P.previo_venta - F.financiamiento) AS "SALDO_PENDIENTE", ' + vendedor + ' AS "VENDEDOR", P.comentario AS "COMENTARIO" FROM "PUESTOS" P, "CLIENTES" C, "FINANCIAMIENTOS" F, "USUARIOS" U WHERE P.fk_cliente = C.dni AND C.fk_usu_venta = U.dni AND P.fk_financiamiento = F.id');
 
         console.log(response)
 
